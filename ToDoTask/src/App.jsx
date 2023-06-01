a8a0aeac03bd3f33d20c90abf44e367b272cc1b6
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-05-31 23:45:58 -0400

    Finish function project
    Montagem das funções completas, ao decorrer do tempo estarei estilizando o programa.

--- ToDoTask/src/App.jsx
+++ ToDoTask/src/App.jsx
@@ -6,26 +6,7 @@ import Search from './components/Search';
 import Filter from './components/Filter';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: "criar funcionalidade x no sistema",
-      category: "Trabalho",
-      isCompleted: false,
-    },
-    {
-      id:2,
-      text: "Ir pra academia",
-      category: "Pessoal",
-      isCompleted: false,
-    },
-    {
-      id:3,
-      text: "Estudar React",
-      category: "Estudos",
-      isCompleted: false,
-    }
-  ])
+  const [todos, setTodos] = useState([])
 
   const [search, setSearch] = useState("")
 
