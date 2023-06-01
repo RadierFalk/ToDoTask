2dae4cdffccb0396b7361e06d9d130d5ccc1a62a
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-05-31 23:39:04 -0400

    Create option filter and Search

--- ToDoTask/src/App.jsx
+++ ToDoTask/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import Todo from './components/todo';
 import "./App.css";
 import TodoForm from './components/TodoForm';
-import Seach from './components/Seach';
+import Search from './components/Search';
+import Filter from './components/Filter';
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -25,7 +27,10 @@ function App() {
     }
   ])
 
-  const [seach, setSeach] = useState("")
+  const [search, setSearch] = useState("")
+
+  const [filter, setFilter] = useState("All")
+  const [sort, setSort] = useState("Asc")
 
   const addTodo = (text, category) => {
     const newTodo = [...todos, {
@@ -53,10 +58,20 @@ function App() {
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
-      <Seach seach={seach} setSeach={setSeach} />
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter = {filter} setFilter = {setFilter} setSort={setSort} />
       <div className='todo-list'>
-        {todos.filter((todo) => todo.text.toLowerCase().includes(seach.toLowerCase())
+        {todos
+        .filter((todo) => filter === "All" 
+        ? true : filter === "Completed" 
+        ? todo.isCompleted 
+        : !todo.isCompleted
         )
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => sort === "Asc" 
+        ? a.text.localeCompare(b.text) 
+        : b.text.localeCompare(a.text))
          .map((todo) => (
           <Todo key={todo.id}
            todo={todo}
