b9031e73824a9e1f00b77e2f878d9680f129bce0
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-05-31 22:12:04 -0400

    Add + Style task
    Montando a função de adicionar tasks a lista e estilizando o projeto.

--- ToDoTask/src/App.jsx
+++ ToDoTask/src/App.jsx
@@ -23,15 +23,27 @@ function App() {
       isCompleted: false,
     }
   ])
+
+  const addTodo = (text, category) => {
+    const newTodo = [...todos, {
+      id: Math.floor(Math.random()*10000),
+      text,
+      category,
+      isCompleted: false,
+    },
+  ]
+
+  setTodos(newTodo)
+  } 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
    
   )
